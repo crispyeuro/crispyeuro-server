ce829289af2ae4aa71a8b825ad61065e59b1ad8b
allavr <allavr@ttu.ee>
2021-03-07 15:00:39 +0200

    fix: insert test data into 'user_account' and 'user_status' tables (crispyeuro/crispyeuro#16)

--- scripts/database.sql
+++ scripts/database.sql
@@ -14,10 +14,67 @@ CREATE TABLE IF NOT EXISTS user_account (
 CREATE TABLE IF NOT EXISTS user_status (
     user_status_id INT GENERATED ALWAYS AS IDENTITY,
     user_id INT UNIQUE NOT NULL,
-    created_on DATE NOT NULL,
+    created_on TIMESTAMP NOT NULL,
     email_verified BOOLEAN,
-    last_login DATE,
+    last_login TIMESTAMP,
     user_active BOOLEAN,
     PRIMARY KEY(user_status_id),
     CONSTRAINT fk_user_id FOREIGN KEY(user_id) REFERENCES user_account(user_id)
 );
+/* Insert test data into user_account
+ INSERT INTO user_account(
+ name,
+ username,
+ password,
+ email,
+ birthday,
+ gender,
+ country,
+ city
+ )
+ VALUES (
+ 'Mari',
+ 'mari247',
+ 'Qw984erty,',
+ 'mari@mari.ee,',
+ '1990-12-23',
+ 'female',
+ 'Estonia',
+ 'Tallinn'
+ );
+ INSERT INTO user_account(username, password, email, country)
+ VALUES (
+ 'rein589h',
+ 'Asdfgh1hjk!t5,',
+ 'rein341@mari.ee,',
+ 'Estonia'
+ );
+ */
+/*Insert test data into user_status*/
+/*INSERT INTO user_status(
+        user_id,
+        created_on,
+        email_verified,
+        last_login,
+        user_active
+    )
+VALUES (
+        1,
+        '2020-03-06 19:20:26-01',
+        true,
+        '2020-03-06 19:23:38-08',
+        true
+    );
+INSERT INTO user_status(
+        user_id, 
+        created_on, 
+        email_verified, 
+        user_active
+    )
+VALUES (
+        2, 
+        '2020-03-06 19:31:28-05', 
+        false, 
+        false
+    );
+*/
