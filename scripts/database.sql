80901651bacf396c8d65cc8306901404fd69600b
allavr <allavr@ttu.ee>
2021-03-12 20:08:03 +0200

    feat: add database tables for 'Coincard' (crispyeuro/crispyeuro#18)

--- scripts/database.sql
+++ scripts/database.sql
@@ -1,3 +1,4 @@
+/*User account*/
 CREATE TABLE IF NOT EXISTS user_account (
     user_id INT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(200),
@@ -10,6 +11,8 @@ CREATE TABLE IF NOT EXISTS user_account (
     address VARCHAR(200),
     PRIMARY KEY(user_id)
 );
+
+/*User account status*/
 CREATE TABLE IF NOT EXISTS user_status (
     user_status_id INT GENERATED ALWAYS AS IDENTITY,
     user_id INT UNIQUE NOT NULL,
@@ -20,3 +23,69 @@ CREATE TABLE IF NOT EXISTS user_status (
     PRIMARY KEY(user_status_id),
     CONSTRAINT fk_user_id FOREIGN KEY(user_id) REFERENCES user_account(user_id)
 );
+
+/*Coin*/
+CREATE TABLE IF NOT EXISTS coin (
+    coin_id INT GENERATED ALWAYS AS IDENTITY,
+    country VARCHAR(15) NOT NULL,
+    issue_year INT NOT NULL,
+    denomination DECIMAL NOT NULL,
+    coin_type VARCHAR(20),
+    obverse_image_path VARCHAR(1000),
+    reverse_image_path VARCHAR(1000),
+    diameter DECIMAL,
+    thickness DECIMAL,
+    mass DECIMAL,
+    composition VARCHAR(300),
+    edge VARCHAR(300),
+    feature VARCHAR(300),
+    coin_description VARCHAR(1000),
+    PRIMARY KEY(coin_id)
+);
+
+/*Coin mintage*/
+CREATE TABLE IF NOT EXISTS coin_mintage (
+    coin_mintage_id INT GENERATED ALWAYS AS IDENTITY,
+    coin_id INT NOT NULL,
+    mintage_total INT,
+    uncirculated INT,
+    brilliant_uncirculated INT,
+    proof INT,
+    mintmark VARCHAR(50),
+    mint VARCHAR(300),
+    issue_date DATE,
+    mintage_description VARCHAR(1000),
+    PRIMARY KEY(coin_mintage_id),
+    CONSTRAINT fk_coin_id FOREIGN KEY(coin_id) REFERENCES coin(coin_id)
+);
+
+
+/*Insert queries*/
+/*Insert test data into 'user_status' table*/
+/*INSERT INTO user_status(user_id, created_on, email_verified, last_login, user_active) VALUES (1, '2020-03-06 19:20:26-01', true, '2020-03-06 19:23:38-08', true);
+INSERT INTO user_status(user_id, created_on, email_verified, user_active) VALUES (2, '2020-03-06 19:31:28-05', false, false);
+*/
+/*Insert coins into 'coins' table*/
+/*INSERT INTO coin(country, issue_year, denomination, coin_type, diameter, thickness, mass, composition, edge) 
+    VALUES ('Estonia', 2011, '0.01', 'ordinary', 16.25, 1.67, 2.3, 'Copper-covered steel', 'Smooth');
+INSERT INTO coin(country, issue_year, denomination, coin_type, diameter, thickness, mass, composition, edge) 
+    VALUES ('Estonia', 2011, '0.02', 'ordinary', 18.75, 1.67, 3.06, 'Copper-covered steel', 'Smooth with a groove');
+INSERT INTO coin(country, issue_year, denomination, coin_type, diameter, thickness, mass, composition, edge) 
+    VALUES ('Estonia', 2011, '0.05', 'ordinary', 21.25, 1.67, 3.92, 'Copper-covered steel', 'Smooth');
+INSERT INTO coin(country, issue_year, denomination, coin_type, diameter, thickness, mass, composition, edge) 
+    VALUES ('Estonia', 2011, '0.1', 'ordinary', 22.25, 2.14, 5.74, 'Nordic gold', 'Shaped edge with file scallops');
+INSERT INTO coin(country, issue_year, denomination, coin_type, diameter, thickness, mass, composition, edge) 
+    VALUES ('Estonia', 2011, '0.2', 'ordinary', 22.25, 2.14, 5.74, 'Nordic gold', 'Plain with seven indents (Spanish flower)');
+INSERT INTO coin(country, issue_year, denomination, coin_type, diameter, thickness, mass, composition, edge) 
+    VALUES ('Estonia', 2011, '0.5', 'ordinary', 24.25, 2.38, 7.8, 'Nordic gold', 'Shaped edge with fine scallops');
+INSERT INTO coin(country, issue_year, denomination, coin_type, diameter, thickness, mass, composition, edge) 
+    VALUES ('Estonia', 2011, '1', 'ordinary', 23.25, 2.33, 7.5, 'Outer part: nickel brass, Inner part: Layers of copper-nickel, nickel, copper-nickel', 'Interrupted milled');
+INSERT INTO coin(country, issue_year, denomination, coin_type, diameter, thickness, mass, composition, edge) 
+    VALUES ('Estonia', 2011, '2', 'ordinary', 25.75, 2.2, 8.5, 'Inner part: copper-nickel, Inner part: Layers of nickel brass, nickel, nickel brass', 'Reeded with inscription: "EESTI O EESTI 0"');
+*/
+/*Insert coins' mintages into 'coin_mintage' table*/
+/*INSERT INTO coin_mintage (coin_id, mintage_total, uncirculated, brilliant_uncirculated, proof, mint) 
+    VALUES (1, 32000000, 50000, 3500, 'Mint of Finland');
+INSERT INTO coin_mintage (coin_id, mintage_total, uncirculated, brilliant_uncirculated, proof, mint) 
+    VALUES (2, 30000000, 50000, 3500, 'Mint of Finland');
+*/
