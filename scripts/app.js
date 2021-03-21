bb017cf329d6a42c186fe866365ee3503751d768
allavr <allavr@ttu.ee>
2021-03-21 21:46:36 +0200

    feat: connect 'coin' database table with client (crispyeuro/crispyeuro#26)

--- scripts/app.js
+++ scripts/app.js
@@ -8,11 +8,9 @@ const app = express();
 
 const client = new pg.Client();
 
-
 async function test() {
     await client.connect();
     await loadDatabaseCreationCode(client);
-    await client.end();
 }
 
 async function loadDatabaseCreationCode(databaseClient) {
@@ -36,6 +34,31 @@ app.get('/', (req, res) => {
     res.redirect('/static');
 })
 
+app.get('/api/first-row', (serverRequest, serverResponse) => {
+    client.query('SELECT * FROM coin WHERE coin_id = $1', [1], (err, databaseResponse) => {
+        if (err) {
+            console.log(err.stack);
+        } else {
+            serverResponse.json(databaseResponse.rows);
+        }
+    })
+});
+
+app.get('/api/lala', (serverRequest, serverResponse) => {
+    const {year, country, nominal} = serverRequest.query;
+    client.query('SELECT * FROM coin WHERE issue_year = $1 AND country = $2 AND denomination = $3', [year, country, nominal], (err, databaseResponse) => {
+        if (err) {
+            console.log(err.stack);
+        } else {
+            serverResponse.json(databaseResponse.rows);
+        }
+    });
+});
+
 app.listen(8080, () => {
     console.log('server is running...');
 })
+
+process.on('exit', function() {
+    client.end();
+});
